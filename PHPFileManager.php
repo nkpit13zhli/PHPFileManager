0fe893de83afecd44b91ed8a8209603a34cae727
Zebh Lindqvist <zebh.lindqvist@outlook.com>
2015-09-21 14:45:52 +0200

    Fixed path calculation

--- PHPFileManager.php
+++ PHPFileManager.php
@@ -4,7 +4,7 @@ class PHPFileManager
 {
 	function delete($path, $filename)
 	{
-		$path = substr($path, -1) == "/" ? $path : $path . "/";
+		$path = substr($path, -1) == "/" or $path != "" ? $path : $path . "/";
 		unlink($path . $filename);
 	}
 }
